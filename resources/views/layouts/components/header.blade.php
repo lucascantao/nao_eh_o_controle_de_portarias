2a5bd9181d0aa5d23beedf140ede033fc372ec0c
José Lucas dos Santos Cantão <cantao162@gmail.com>
2024-04-16 09:43:56 -0300

    botão perfil do header

--- resources/views/layouts/components/header.blade.php
+++ resources/views/layouts/components/header.blade.php
@@ -3,9 +3,11 @@
         <span id="logo" ><a href="/"><img src="{{asset('images/SiCop-v1.png')}}" width="128px" alt=""></a></span>
         <span id="titulo" class="fs-5" style="font-weight: bold"><span style="font-weight: normal" class="text-secondary">Não é </span>Controle de Portaria</span>
         <div class="me-3">
-            <i class="bi bi-person-circle"></i>
-            <span style="font-weight: bold; font-size: 14px">Usuario</span>
-            <a id="button" class="btn btn-primary px-4 py-1" href="/">Sair</a>
+            <a href="{{route('profile.edit')}}">
+                <i class="bi bi-person-circle"></i>
+                <span style="font-weight: bold; font-size: 14px">{{ Auth::user()->name }}</span>
+            </a>
+            <a id="button" class="btn btn-primary px-4 py-1" href="{{route('logout')}}">Sair</a>
         </div>
     </div>
 </nav>
