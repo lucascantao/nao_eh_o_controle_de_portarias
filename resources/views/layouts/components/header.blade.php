bd956f7c349f341365fdd9e5f3e5eb73b2ff3888
José Lucas dos Santos Cantão <cantao162@gmail.com>
2024-04-16 08:15:40 -0300

    teste

--- resources/views/layouts/components/header.blade.php
+++ resources/views/layouts/components/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg sticky-top py-3 bg-white">
     <div class="d-flex justify-content-between align-items-center w-100">
         <span id="logo" ><a href="/"><img src="{{asset('images/SiCop-v1.png')}}" width="128px" alt=""></a></span>
-        <span id="titulo" class="fs-5" style="font-weight: bold">Controle de Portaria</span>
+        <span id="titulo" class="fs-5" style="font-weight: bold">´Não é o Controle de Portaria</span>
         <div class="me-3">
             <i class="bi bi-person-circle"></i>
             <span style="font-weight: bold; font-size: 14px">Usuario</span>
