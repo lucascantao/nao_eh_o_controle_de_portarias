63bf16f0434f12a4c1e0dceca90f2fba90623c3d
José Lucas dos Santos Cantão <cantao162@gmail.com>
2024-04-16 08:23:06 -0300

    título

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Controle de Portarias</title>
+        <title>Não é Controle de Portarias</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
